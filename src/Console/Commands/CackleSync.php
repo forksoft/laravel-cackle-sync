79a8a8b64a1cd6cbbff005a9df75c52c1068f26d
Artem Sychev <temph69@gmail.com>
2023-02-25 17:37:43 +0300

    command beautify

--- src/Console/Commands/CackleSync.php
+++ src/Console/Commands/CackleSync.php
@@ -8,10 +8,11 @@ use Aleksei4er\LaravelCackleSync\Jobs\LoadReviews;
 use Aleksei4er\LaravelCackleSync\Models\CackleComment;
 use Aleksei4er\LaravelCackleSync\Models\CackleReview;
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 
 class CackleSync extends Command
 {
-    protected $signature = 'cackle:sync';
+    protected $signature = 'cackle:sync {--type=*}';
 
     /**
      * Execute the console command.
@@ -20,12 +21,36 @@ class CackleSync extends Command
      */
     public function handle()
     {
-        LoadChannels::dispatch()->onQueue("cackle");
+        $types = $this->getTypes();
+        if (empty($types)) {
+            return;
+        }
 
-        $lastCommentId = CackleComment::max('id') ?? 0;
-        LoadComments::dispatch($lastCommentId)->onQueue("cackle");
+        LoadChannels::dispatch()->onQueue('cackle');
 
-        $modified = CackleReview::max('modified') ?? 0;
-        LoadReviews::dispatch($modified)->onQueue("cackle");
+        if (\in_array('comments', $types, true)) {
+            $lastCommentId = CackleComment::max('id') ?? 0;
+            LoadComments::dispatch($lastCommentId)->onQueue('cackle');
+        }
+
+        if (\in_array('reviews', $types, true)) {
+            $modified = CackleReview::max('modified') ?? 0;
+            LoadReviews::dispatch($modified)->onQueue('cackle');
+        }
+    }
+
+    protected function getTypes(): array
+    {
+        $types = $this->option('type');
+
+        if (!\is_array($types)) {
+            $types = explode(',', (string) $types);
+        }
+
+        if (\count($types) === 1 && Str::contains($types[0], ',')) {
+            $types = explode(',', (string) $types[0]);
+        }
+
+        return $types;
     }
 }
